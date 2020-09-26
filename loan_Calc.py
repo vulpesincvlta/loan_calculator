88419bf1389239d4a9cc81cd18e7d0c089b1ff36
paddybean <miguel.a.b.bueno@gmail.com>
2020-09-25 22:43:23 -0500

    lawl

--- loan_Calc.py
+++ loan_Calc.py
@@ -1,35 +1,57 @@
 import math
 
-print('Enter the loan principal: ')
 
-loan_principal = int(input())
 
-print(f'What do you want to calculate? \ntype "m" - for number of monthly payments,\ntype "p" - for the monthly payment:')
-
+print(f'What do you want to calculate? \ntype "n" for number of monthly payments, \ntype "a" for annuity monthly payment amount,\ntype "p" for loan principal:')
 a = input()
 
-if a == 'm':
+if a == 'n':
 
-    print('Enter the monthly payment: ')
-    payment = int(input())
-    pay_time = loan_principal / payment
-    t = math.ceil(pay_time)
-    if t == 1:
-        print(f'It will take {t} month to repay the loan')
+    print('Enter the loan principal:')
+    loan_principal = int(input())
+
+    print('Enter the monthly payment:')
+    monthly_payment = int(input())
+
+    print('Enter the loan interest:')
+    li = float(input())
+
+    ni = li / (12 * 100)
+    fom = monthly_payment / (monthly_payment - ni * loan_principal)
+    num_months = math.log(fom , ni + 1 )
+    rounded = math.ceil(num_months)
+
+    if rounded % 12 == 0:
+        print(f'It will take {rounded // 12} years to repay this loan!')
     else:
-        print(f'It will take {t} months to repay the loan')
+        print(f'It will take {rounded // 12} years and {rounded % 12} months to repay this loan!')
+
+elif a == 'a':
+    print('Enter the loan principal:')
+    lp = int(input())
+    print('Enter the number of periods:')
+    np = int(input())
+    print('Enter the loan interest:')
+    li = float(input())
+    i = li / (12 * 100)
+    p = (1+ i) **  np
+    mp = lp * ((i * p)/(p - 1))
+    print(f'Your monthly payment = {math.ceil(mp)}!')
 
 elif a == 'p':
-    print('Enter the number of months:')
-    months = int(input())
-    month_pay = loan_principal / months
 
-    last = loan_principal - ((months - 1) * math.ceil(month_pay))
+    print('Enter the annuity payment:')
+    ap = float(input())
+    print('Enter the number of periods:')
+    np = int(input())
+    print('Enter the loan interest:')
+    li = float(input())
 
-    if loan_principal % months == 0:
-        print(f'Your monthly payment = {math.ceil(month_pay)}')
-    else:
-        print(f'Your monthly payment = {math.ceil(month_pay)} and the last payment = {last}.')
+    i = li / (12 * 100)
+    p = (1+ i) **  np
+
+    lp = ap / ((i * p)/(p - 1))
+    print(f'Your loan principal = {math.floor(lp)}!')
 
 
 else:
