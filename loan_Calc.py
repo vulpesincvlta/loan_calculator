d22fcb55910baf85fae38ccfaa8051cfe3514edc
paddybean <miguel.a.b.bueno@gmail.com>
2020-09-29 23:17:00 -0500

    new functions added

--- loan_Calc.py
+++ loan_Calc.py
@@ -1,58 +1,57 @@
+import argparse
 import math
+import sys
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--type", help="choose between loan calculation")
+parser.add_argument("--payment", help="monthly payment amount")
+parser.add_argument("--principal", help="loan principal value")
+parser.add_argument("--periods", help=" denotes the number of months needed to repay the loan")
+parser.add_argument("--interest", help="loan interest")
 
-print(f'What do you want to calculate? \ntype "n" for number of monthly payments, \ntype "a" for annuity monthly payment amount,\ntype "p" for loan principal:')
-a = input()
+args = parser.parse_args()
 
-if a == 'n':
-
-    print('Enter the loan principal:')
-    loan_principal = int(input())
-
-    print('Enter the monthly payment:')
-    monthly_payment = int(input())
-
-    print('Enter the loan interest:')
-    li = float(input())
-
-    ni = li / (12 * 100)
-    fom = monthly_payment / (monthly_payment - ni * loan_principal)
-    num_months = math.log(fom , ni + 1 )
-    rounded = math.ceil(num_months)
-
-    if rounded % 12 == 0:
-        print(f'It will take {rounded // 12} years to repay this loan!')
-    else:
-        print(f'It will take {rounded // 12} years and {rounded % 12} months to repay this loan!')
-
-elif a == 'a':
-    print('Enter the loan principal:')
-    lp = int(input())
-    print('Enter the number of periods:')
-    np = int(input())
-    print('Enter the loan interest:')
-    li = float(input())
-    i = li / (12 * 100)
-    p = (1+ i) **  np
-    mp = lp * ((i * p)/(p - 1))
-    print(f'Your monthly payment = {math.ceil(mp)}!')
-
-elif a == 'p':
-
-    print('Enter the annuity payment:')
-    ap = float(input())
-    print('Enter the number of periods:')
-    np = int(input())
-    print('Enter the loan interest:')
-    li = float(input())
-
-    i = li / (12 * 100)
-    p = (1+ i) **  np
-
-    lp = ap / ((i * p)/(p - 1))
-    print(f'Your loan principal = {math.floor(lp)}!')
-
-
+if len(sys.argv) < 4:
+    print('Incorrect parameters')
 else:
-    print('error')
+    if args.interest is None:
+        print('Incorrect parameters')
+    elif args.interest is not None:
+        if args.type == 'diff':
+            lp = int(args.principal)  # loan principal
+            li = int(args.interest) # interest
+            payments = int(args.periods) # number of payments
+            i = li / (12 * 100)
+            op = 0
+            for m in range(payments):
+                diffp = (lp / payments) +  (i * (lp - ((lp * ((m + 1) - 1))/(payments))))
+                print(f'Month {m + 1}: payment is {math.ceil(diffp)}')
+                op += math.ceil(diffp)
+                print(f'Overpayment: {math.ceil(op) - lp}')
+        elif args.type == 'annuity':
+            if args.payment is not None:
+                ap = int(args.payment)
+                li = float(args.interest)
+                np = int(args.periods)
+                i = li / (12 * 100)
+                p = (1 + i) **  np
+                lp = ap / ((i * p)/(p - 1))
+                print(f'Your loan principal = {math.floor(lp)}!')
+                op = (ap * np) - lp
+                print(f'Overpayment: {math.ceil(op)}')
+            elif args.payment is None:
+                lp = int(args.principal)
+                ap = int(args.payment)
+                li = float(args.interest)
+                i = li / (12 * 100)
+                fom = ap / (ap - ni * lp)
+                num_months = math.log(fom , li + 1 )
+                rounded = math.ceil(num_months)
+                op = (rounded * monthly_payment) - lp
+                if rounded % 12 == 0:
+                    print(f'It will take {rounded // 12} years to repay this loan! \nOverpayment: {math.ceil(op)}')
+                else:
+                    print(f'It will take {rounded // 12} years and {rounded % 12} months to repay this loan! \nOverpayment: {math.ceil(op)}')
+            else:
+                print('Incorrect parameters')
